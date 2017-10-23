3c79d6374e5ae40a2b5cc7d0e6e3f182bbbb33f8
linzhenxiang <linzhenxiang1@126.com>
2017-10-23 20:12:11 +0800

    小壳 txt全本小说阅读器 合并到中壳

--- common_book/src/txtqbxsydq/java/com/intelligent/reader/activity/SettingActivity.java
+++ common_book/src/txtqbxsydq/java/com/intelligent/reader/activity/SettingActivity.java
@@ -1,5 +1,23 @@
 package com.intelligent.reader.activity;
 
+import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
+import com.bumptech.glide.Glide;
+import com.intelligent.reader.R;
+import com.intelligent.reader.util.EventBookStore;
+
+import net.lzbook.kit.app.BaseBookApplication;
+import net.lzbook.kit.appender_loghub.StartLogClickUtil;
+import net.lzbook.kit.book.component.service.DownloadService;
+import net.lzbook.kit.book.view.MyDialog;
+import net.lzbook.kit.book.view.SwitchButton;
+import net.lzbook.kit.cache.DataCleanManager;
+import net.lzbook.kit.constants.Constants;
+import net.lzbook.kit.data.bean.BookTask;
+import net.lzbook.kit.utils.AppUtils;
+import net.lzbook.kit.utils.StatServiceUtils;
+import net.lzbook.kit.utils.UIHelper;
+import net.lzbook.kit.utils.update.ApkUpdateUtils;
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -18,23 +36,6 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
-import com.intelligent.reader.R;
-import com.intelligent.reader.util.EventBookStore;
-
-import net.lzbook.kit.app.BaseBookApplication;
-import net.lzbook.kit.appender_loghub.StartLogClickUtil;
-import net.lzbook.kit.book.component.service.DownloadService;
-import net.lzbook.kit.book.view.MyDialog;
-import net.lzbook.kit.book.view.SwitchButton;
-import net.lzbook.kit.cache.DataCleanManager;
-import net.lzbook.kit.constants.Constants;
-import net.lzbook.kit.data.bean.BookTask;
-import net.lzbook.kit.utils.AppUtils;
-import net.lzbook.kit.utils.StatServiceUtils;
-import net.lzbook.kit.utils.UIHelper;
-import net.lzbook.kit.utils.update.ApkUpdateUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -70,6 +71,7 @@ public class SettingActivity extends BaseCacheableActivity implements View.OnCli
     private MyDialog myDialog;//清除缓存对话框
     private RelativeLayout user_login_layout;
     private View is_show_drawable;
+
     private List<RelativeLayout> mRelativeLayoutList;
     private List<TextView> mTextViewList;
     private List<View> mDivider;
@@ -84,6 +86,7 @@ public class SettingActivity extends BaseCacheableActivity implements View.OnCli
     private TextView text_check_update;
     private TextView text_clear_cache;
     private TextView text_disclaimer_statement;
+
     //第二种布局 登录在左侧
     private RelativeLayout top_navigation_bg;
     private ImageView icon_more_left;
@@ -92,6 +95,7 @@ public class SettingActivity extends BaseCacheableActivity implements View.OnCli
     private TextView top_navigation_title;
     private ImageView iv_mine_image_left;
     private RelativeLayout user_login_layout_left;
+
     private LinearLayout rl_setting_layout;//背景
     private RelativeLayout rl_readpage_bbs;//论坛
     private RelativeLayout rl_style_change;//主题切换
@@ -109,6 +113,7 @@ public class SettingActivity extends BaseCacheableActivity implements View.OnCli
     private TextView clear_cache_size;//缓存
     private TextView theme_name;//主题名
     private CacheAsyncTask cacheAsyncTask;
+
     private boolean isActivityPause = false;
     private boolean isStyleChanged = false;
     private Runnable feedbackRunnable = new Runnable() {
