16bf21c130f229a044f3b98a1378718044f0530a
yuchao_liu <yuchao_liu@dingyuegroup.cn>
2017-11-08 18:50:23 +0800

    测试环境章节host

--- book_kit/src/main/java/net/lzbook/kit/repair_books/RepairHelp.java
+++ book_kit/src/main/java/net/lzbook/kit/repair_books/RepairHelp.java
@@ -209,6 +209,7 @@ public class RepairHelp {
 
             try {
                 String url = UrlUtils.buildContentUrl(chapter.curl);
+                url = url.replace("https://www.mantantd.cn", "http://test5.api.bookapi.cn:8090");
                 chapter.content = BaseBookApplication.getGlobalContext().getMainExtractorInterface().extract(url);
                 if (!TextUtils.isEmpty(chapter.content)) {
                     chapter.content = chapter.content.replace("\\n", "\n");
