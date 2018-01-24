88dd73702d2bed96577b4a14146dc881ae3845cf
LeeTecth <jun_li@dingyuegroup.cn>
2018-01-24 10:49:15 +0800

    git test

---
+++ common_book/src/main/java/com/intelligent/reader/read/page/ReaderTest.java
@@ -0,0 +1,11 @@
+package com.intelligent.reader.read.page;
+
+/**
+ * @author lijun Lee
+ * @desc 请描述这个文件
+ * @mail jun_li@dingyuegroup.cn
+ * @data 2018/1/24 10:48
+ */
+
+public class ReaderTest {
+}
