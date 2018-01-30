03b135a88c2af51396b6cf5646b055baba7bac9e
qiantao <tao_qian@dingyuegroup.cn>
2018-01-30 14:50:00 +0800

    广告动态参数 闪屏页广告修复 阅读页开关补全

--- common_book/src/main/java/com/intelligent/reader/read/page/HorizontalPage.kt
+++ common_book/src/main/java/com/intelligent/reader/read/page/HorizontalPage.kt
@@ -24,11 +24,10 @@ import kotlinx.android.synthetic.main.error_page2.view.*
 import kotlinx.android.synthetic.main.loading_page_reading.view.*
 import kotlinx.android.synthetic.main.read_bottom.view.*
 import kotlinx.android.synthetic.main.read_top.view.*
-import net.lzbook.kit.app.BaseBookApplication
+import net.lzbook.kit.constants.Constants
 import net.lzbook.kit.data.bean.Chapter
 import net.lzbook.kit.data.bean.ReadConfig
 import net.lzbook.kit.data.bean.ReadViewEnums
-import net.lzbook.kit.data.db.BookChapterDao
 import net.lzbook.kit.utils.AppUtils
 import net.lzbook.kit.utils.runOnMain
 import net.lzbook.kit.utils.subscribekt
@@ -505,11 +504,16 @@ class HorizontalPage : FrameLayout, Observer {
                             postInvalidate()
                         }
                         //判断展示Banner广告
+
                         val topMargin = if (mNovelPageBean?.lines?.isNotEmpty() == true) mNovelPageBean!!.height else ReadConfig.screenHeight.toFloat()
                         if (ReadConfig.screenHeight - topMargin > ReadConfig.screenHeight / 5) {
-                            hasAd = true
-                            checkAdBanner(topMargin)
+                            hasAd = false
+                            if (!Constants.isHideAD) {
+                                checkAdBanner(topMargin)
+                                hasAd = true
+                            }
                         }
+
                     }
                     changeCursorState(chapterList)
 
