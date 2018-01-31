18519ea1fc668276fe43a1c8ca69738881103d7d
qiantao <tao_qian@dingyuegroup.cn>
2018-01-31 15:31:04 +0800

    解决上下滑动模式下NavigationBar不能隐藏的问题

--- book_kit/src/main/java/iyouqu/theme/FrameActivity.java
+++ book_kit/src/main/java/iyouqu/theme/FrameActivity.java
@@ -71,8 +71,7 @@ public abstract class FrameActivity extends AppCompatActivity implements SwipeBa
     private String packageName;
 
 
-    public static int UI_OPTIONS_IMMERSIVE_STICKY = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    public static int UI_OPTIONS_IMMERSIVE_STICKY = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
             | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
