6da917ceb19e82e94940b48c13eabe6948d4d482
qiantao <tao_qian@dingyuegroup.cn>
2018-03-06 11:46:03 +0800

    update: 「使用协议」界面

--- common_book/src/main/java/com/intelligent/reader/activity/DisclaimerActivity.java
+++ common_book/src/main/java/com/intelligent/reader/activity/DisclaimerActivity.java
@@ -13,20 +13,19 @@ import java.util.Map;
 
 
 public class DisclaimerActivity extends iyouqu.theme.FrameActivity {
-    private boolean isFromReadingPage = false;
 
     @Override
     public void onCreate(Bundle paramBundle) {
         super.onCreate(paramBundle);
         setContentView(R.layout.act_disclaimer);
         if (getIntent() != null) {
-            isFromReadingPage = getIntent().getBooleanExtra("isFromReadingPage", false);
+            boolean isFromReadingPage = getIntent().getBooleanExtra("isFromReadingPage", false);
             if (isFromReadingPage) {
-                ((TextView) findViewById(R.id.tv_title_name)).setText(getResources().getString(R.string.translate_code_disclaimer));
-                ((TextView) findViewById(R.id.tv_disclaimer_statement)).setText(getResources().getString(R.string.translate_code_description));
+                ((TextView) findViewById(R.id.txt_head_title)).setText(getResources().getString(R.string.translate_code_disclaimer));
+                ((TextView) findViewById(R.id.txt_content)).setText(getResources().getString(R.string.translate_code_description));
             }
         }
-        findViewById(R.id.btn_left_setting).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.img_head_back).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Map<String, String> data = new HashMap<>();
