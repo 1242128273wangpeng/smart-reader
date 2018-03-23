7396a5f07fc2a1373c667d095ace6078df2da64a
LeeTecth <jun_li@dingyuegroup.cn>
2018-03-23 11:58:29 +0800

    1.清除残余活跃Activity

--- book_kit/src/main/java/net/lzbook/kit/data/bean/Chapter.java
+++ book_kit/src/main/java/net/lzbook/kit/data/bean/Chapter.java
@@ -1,10 +1,6 @@
 package net.lzbook.kit.data.bean;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 import java.io.Serializable;
-import java.util.ArrayList;
 
 public class Chapter implements Serializable {
 
