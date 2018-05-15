da33a6414069dbdd9d14a4d99b73b3294e2aafd1
linzhenxiang <linzhenxiang1@126.com>
2018-05-15 18:29:36 +0800

    txt电子书 + txt全本免费小说 转Kotlin + MVC -> MVP

---
+++ bookshelf/src/txtqbmfxs/java/com/dingyue/bookshelf/BookShelfInterface.kt
@@ -0,0 +1,16 @@
+package com.dingyue.bookshelf
+
+/**
+ * Desc 提供给 HomeActivity
+ * Author zhenxiang
+ * 2018\5\15 0015
+ */
+
+interface BookShelfInterface {
+
+    fun changeHomeNavigationState(state: Boolean)
+
+    fun changeHomePagerIndex(index: Int)
+
+    fun changeDrawerLayoutState()
+}
