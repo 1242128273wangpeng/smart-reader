de32332d6b75cff560c85f33655d3e2933a2ed2e
CrazyLei <wanglei_wang@dingyuegroup.cn>
2018-06-13 19:57:37 +0800

    1.修改问题

--- book_kit/src/main/java/com/dingyue/contract/BasePopup.kt
+++ book_kit/src/main/java/com/dingyue/contract/BasePopup.kt
@@ -23,9 +23,6 @@ open class BasePopup(var context: Context, var layout: Int, var width: Int, var 
         popupWindow.setBackgroundDrawable(ColorDrawable(0x00000000))
         popupWindow.width = width
         popupWindow.height = height
-
-        popupWindow.isFocusable = true
-        popupWindow.isOutsideTouchable = false
     }
 
     fun showAsDropDown(view: View, x: Int = 0, y: Int = 0) {
