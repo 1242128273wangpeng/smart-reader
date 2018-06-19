6d397492f4ebf9f7cd3fc77c7db5ecd448b97f86
CrazyLei <wanglei_wang@dingyuegroup.cn>
2018-06-19 15:00:46 +0800

    1.添加全本免费热门小说

---
+++ bookshelf/src/qbmfrmxs/java/com/dingyue/bookshelf/BookShelfItemHolder.kt
@@ -0,0 +1,81 @@
+package com.dingyue.bookshelf
+
+import android.support.v7.widget.RecyclerView
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.ding.basic.bean.Book
+import kotlinx.android.synthetic.qbmfrmxs.item_bookshelf_book.view.*
+
+import net.lzbook.kit.constants.ReplaceConstants
+
+/**
+ * Desc 书架页Item
+ * Author qiantao
+ * Mail tao_qian@dingyuegroup.cn
+ * Date 2018/3/5 0002 14:19
+ */
+class BookShelfItemHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_bookshelf_book, parent, false)) {
+
+    fun bind(book: Book, bookshelfItemListener: BookShelfAdapter.BookShelfItemListener,
+             contains: Boolean, remove: Boolean) = with(itemView) {
+
+        if (!TextUtils.isEmpty(book.name)) {
+            this.txt_book_name.text = book.name
+        }
+
+        if (book.sequence + 1 > book.chapter_count) {
+            book.sequence = book.chapter_count - 1
+        }
+
+        when {
+            book.update_status == 1 -> { //更新
+                img_book_status.visibility = View.VISIBLE
+                img_book_status.setImageResource(R.drawable.bookshelf_item_book_update_icon)
+            }
+
+            book.status == "FINISH" -> { //完结
+                img_book_status.visibility = View.VISIBLE
+                img_book_status.setImageResource(R.drawable.bookshelf_item_book_finish_icon)
+            }
+            else -> img_book_status.visibility = View.GONE
+        }
+
+        if (!TextUtils.isEmpty(book.img_url) && book.img_url != ReplaceConstants.getReplaceConstants().DEFAULT_IMAGE_URL) {
+            Glide.with(itemView.context.applicationContext)
+                    .load(book.img_url)
+                    .placeholder(R.drawable.common_book_cover_default_icon)
+                    .error(R.drawable.common_book_cover_default_icon)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(this.img_book_cover)
+        } else {
+            Glide.with(itemView.context.applicationContext)
+                    .load(R.drawable.common_book_cover_default_icon)
+                    .into(this.img_book_cover)
+        }
+
+        if (remove) {
+            this.img_book_select_state.visibility = View.VISIBLE
+            if (contains) {
+                this.img_book_select_state.setBackgroundResource(R.drawable.bookshelf_item_checked_icon)
+            } else {
+                this.img_book_select_state.setBackgroundResource(R.drawable.bookshelf_item_check_icon)
+            }
+        } else {
+            this.img_book_select_state.visibility = View.GONE
+        }
+
+        rl_book_content.setOnClickListener {
+            bookshelfItemListener.clickedBookShelfItem(book, adapterPosition)
+        }
+
+        rl_book_content.setOnLongClickListener {
+            bookshelfItemListener.longClickedBookShelfItem()
+        }
+    }
+}
---
+++ common_book/src/qbmfrmxs/java/com/intelligent/reader/widget/DownloadWaitingWifiDialog.kt
@@ -0,0 +1,58 @@
+package com.intelligent.reader.widget
+
+import android.app.Activity
+import android.view.Gravity
+import com.intelligent.reader.R
+import kotlinx.android.synthetic.txtqbmfyd.dialog_clear_cache.*
+import net.lzbook.kit.book.view.MyDialog
+import net.lzbook.kit.data.bean.BookTask
+
+
+/**
+ * Desc 非 wifi 环境下载提醒
+ * Author qiantao
+ * Mail tao_qian@dingyuegroup.cn
+ * Date 2018/3/1 0002 15:09
+ */
+class DownloadWaitingWifiDialog(val activity: Activity) {
+
+    private val dialog = MyDialog(activity, R.layout.dialog_wifi_warning, Gravity.BOTTOM)
+
+    private var list: List<BookTask>? = null
+
+    private var confirmListener: ((list: List<BookTask>?) -> Unit)? = null
+
+    init {
+
+        val window = dialog.window
+        window.setWindowAnimations(R.style.BottomPopupDialog)
+
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.setCancelable(true)
+
+        dialog.btn_confirm.setOnClickListener {
+            dialog.dismiss()
+            confirmListener?.invoke(list)
+        }
+
+        dialog.btn_cancel.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+
+    fun show(list: List<BookTask>) {
+        this.list = list
+        dialog.show()
+    }
+
+    fun dismiss() {
+        dialog.dismiss()
+    }
+
+    fun setOnConfirmListener(listener: ((list: List<BookTask>?) -> Unit)) {
+        confirmListener = listener
+    }
+
+    fun isShowing(): Boolean = dialog.isShowing
+
+}
