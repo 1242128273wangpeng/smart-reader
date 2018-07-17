2fb7f01a58b83c9136e25d529ae2599907e45498
JoannChen <yongzuo_chen@dingyuegroup.cn>
2018-07-17 16:43:27 +0800

    智胜电子书替 书籍详情页修改启动模式为SingleTop 界面细节调整

--- common_book/src/txtqbdzs/java/com/intelligent/reader/activity/CoverPageActivity.kt
+++ common_book/src/txtqbdzs/java/com/intelligent/reader/activity/CoverPageActivity.kt
@@ -96,6 +96,10 @@ class CoverPageActivity : BaseCacheableActivity(), OnClickListener, CoverPageCon
     }
 
     override fun onNewIntent(intent: Intent) {
+        if (book_cover_bookshelf != null) {
+            book_cover_bookshelf!!.isClickable = true
+            insertBookShelfResult(false)
+        }
         initIntent(intent)
     }
 
