5a79bf56f68e830822433696e6a7040ef1dd9298
Danny.Z <svip.xian@gmail.com>
2018-07-19 11:23:04 +0800

    调整广告渠道号,UDID的初始化时机

--- common_book/src/main/java/com/intelligent/reader/app/BookApplication.java
+++ common_book/src/main/java/com/intelligent/reader/app/BookApplication.java
@@ -62,6 +62,8 @@ public class BookApplication extends BaseBookApplication {
 
         if (AppUtils.isMainProcess(this)) {
 
+            MediaConfig.INSTANCE.setAd_userid(OpenUDID.getOpenUDIDInContext(BaseBookApplication.getGlobalContext()));
+            MediaConfig.INSTANCE.setChannel_code(AppUtils.getChannelId());
             MediaLifecycle.INSTANCE.onAppCreate(this);
 
             //防止定位不回掉导致缺失id
