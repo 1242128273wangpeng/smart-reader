b45008bf807a2b8655525aa0e05b42cb6558dd4c
linzhenxiang <linzhenxiang1@126.com>
2018-07-30 14:42:11 +0800

    免费小说全本阅读 书架页模块

---
+++ bookshelf/src/mfxsqbyd/java/com/dingyue/bookshelf/view/HeadMenuPopup.kt
@@ -0,0 +1,51 @@
+package com.dingyue.bookshelf.view
+
+import android.content.Context
+import android.view.View
+import android.view.WindowManager
+import com.dingyue.bookshelf.R
+import com.dingyue.contract.BasePopup
+import kotlinx.android.synthetic.mfxsqbyd.popup_head_menu.view.*
+
+/**
+ * Desc 请描述这个文件
+ * Author qiantao
+ * Mail tao_qian@dingyuegroup.cn
+ * Date 2018/3/6 0006 10:36
+ */
+class HeadMenuPopup(context: Context, layout: Int = R.layout.popup_head_menu,
+                    width: Int = WindowManager.LayoutParams.WRAP_CONTENT,
+                    height: Int = WindowManager.LayoutParams.WRAP_CONTENT)
+    : BasePopup(context, layout, width, height) {
+
+    private var downloadListener: (() -> Unit)? = null
+    private var sortingListener: (() -> Unit)? = null
+
+    init {
+        popupWindow.isFocusable = true
+        popupWindow.isOutsideTouchable = false
+
+        contentView.ll_download_manager.setOnClickListener {
+            dismiss()
+            downloadListener?.invoke()
+        }
+
+        contentView.ll_book_sorting.setOnClickListener {
+            dismiss()
+            sortingListener?.invoke()
+        }
+
+    }
+
+    fun setOnDownloadClickListener(listener: (() -> Unit)) {
+        downloadListener = listener
+    }
+
+    fun setOnSortingClickListener(listener: (() -> Unit)) {
+        sortingListener = listener
+    }
+
+    fun show(view: View) {
+        showAsDropDown(view)
+    }
+}
