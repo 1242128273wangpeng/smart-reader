21e106a9ef57bb20036894dc54b1c55e6e319096
JoannChen <yongzuo_chen@dingyuegroup.cn>
2018-08-07 15:18:56 +0800

    全本免费快看阅读器 书架子条目背景优化

--- bookshelf/src/qbmfkkydq/java/com/dingyue/bookshelf/BookShelfADDHolder.kt
+++ bookshelf/src/qbmfkkydq/java/com/dingyue/bookshelf/BookShelfADDHolder.kt
@@ -1,10 +1,9 @@
 package com.dingyue.bookshelf
 
-import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import net.lzbook.kit.utils.AppUtils
+import kotlinx.android.synthetic.qbmfkkydq.item_bookshelf_add.view.*
 
 /**
  * Desc 书架添加书籍Item
@@ -13,11 +12,22 @@ import net.lzbook.kit.utils.AppUtils
  * Date 2018/5/10 21:07
  */
 class BookShelfADDHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
-    LayoutInflater.from(parent.context).inflate(R.layout.item_bookshelf_add, parent, false)) {
+        LayoutInflater.from(parent.context).inflate(R.layout.item_bookshelf_add, parent, false)) {
 
-    fun bind(size:Int,itemListener: BookShelfAdapter.BookShelfItemListener) = with(itemView) {
+    fun bind(size: Int, itemListener: BookShelfAdapter.BookShelfItemListener) = with(itemView) {
+
+        /**
+         * 书架背景优化
+         * http://note.youdao.com/noteshare?id=7b6be9f7706849746a83d21a207d3ca5&sub=BB3CD2D7346043C59050C702C72D3FEB
+         */
+        if (size % 3 == 0) {
+            fl_add_layout.setBackgroundResource(R.color.color_white)
+        } else {
+            fl_add_layout.setBackgroundResource(R.drawable.bookshelf_bookitem_bg)
+        }
+
         itemView.setOnClickListener {
-            itemListener?.clickedBookShelfItem(null,adapterPosition)
+            itemListener.clickedBookShelfItem(null, adapterPosition)
         }
     }
 }
