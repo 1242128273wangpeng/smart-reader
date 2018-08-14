60428f2b7e6717facf26c5827b5d5a57d90d3dc7
JoannChen <yongzuo_chen@dingyuegroup.cn>
2018-08-14 17:29:33 +0800

    阅微替 解决Fragment already added异常

--- dy-reader/src/zsmfqbxs/java/com/dy/reader/fragment/ReadSettingFragment.kt
+++ dy-reader/src/zsmfqbxs/java/com/dy/reader/fragment/ReadSettingFragment.kt
@@ -182,6 +182,7 @@ class ReadSettingFragment : DialogFragment() , CallBackDownload {
     fun show(flag: Boolean) {
         try {
             if (flag && !this.isAdded && null == fm?.findFragmentByTag(TAG)) {
+                fm?.beginTransaction()?.remove(this)?.commit()
                 this.show(fm, TAG)
             } else {
                 dismiss()
