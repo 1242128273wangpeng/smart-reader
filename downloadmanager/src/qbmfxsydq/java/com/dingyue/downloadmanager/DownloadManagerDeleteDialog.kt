ee1dc0ef8b4932462c10b21c687ad10844c215b0
yuchao_liu <yuchao_liu@dingyuegroup.cn>
2018-08-14 10:15:54 +0800

    五步替: 缓存管理 删除 改为 删除缓存; 清除loading对话框样式调整

--- downloadmanager/src/qbmfxsydq/java/com/dingyue/downloadmanager/DownloadManagerDeleteDialog.kt
+++ downloadmanager/src/qbmfxsydq/java/com/dingyue/downloadmanager/DownloadManagerDeleteDialog.kt
@@ -9,15 +9,9 @@ import net.lzbook.kit.book.view.MyDialog
  */
 class DownloadManagerDeleteDialog(activity: Activity) {
 
-    private val dialog = MyDialog(activity, R.layout.dialog_download_manager_clear_loading)
+    private val dialog = MyDialog(activity, R.layout.dialog_download_manager_clear)
 
     init {
-        val window = dialog.window
-        val layoutParams = window.attributes
-        layoutParams.width = FrameLayout.LayoutParams.MATCH_PARENT
-        layoutParams.height = FrameLayout.LayoutParams.MATCH_PARENT
-        window.attributes = layoutParams
-        window.setWindowAnimations(R.style.BottomPopupDialog)
         dialog.setCancelable(false)
     }
     fun show() {
