06ae0f22ced3e636d77a3ea210957da7c941312c
钱涛 <tao_qian@dingyuegroup.cn>
2018-08-15 17:13:19 +0800

    还原动态参数的梳理
    (cherry picked from commit ac88bdb)

--- basic/src/main/java/com/ding/basic/request/RequestService.kt
+++ basic/src/main/java/com/ding/basic/request/RequestService.kt
@@ -227,10 +227,10 @@ interface RequestService {
     fun requestDynamicCheck(): Flowable<BasicResult<Int>>
 
     @GET(DYNAMIC_PARAMETERS)
-    fun requestDynamicParameters(): Flowable<Parameter>
+    fun requestDynamicParameters(): Flowable<JsonObject>
 
     @GET
-    fun requestCDNDynamicPar(@Url url: String): Flowable<Parameter>
+    fun requestCDNDynamicPar(@Url url: String): Flowable<JsonObject>
 
     @GET(BOOK_DETAIL)
     fun requestBookDetail(@Query("book_id") book_id: String, @Query("book_source_id") book_source_id: String): Flowable<BasicResult<Book>>
