4ef34e6826048ca6df452b7cb78844037aca3cc7
JoannChen <yongzuo_chen@dingyuegroup.cn>
2018-08-22 15:29:02 +0800

    TXT全本免费阅读 删除book-kit下废弃代码

--- book_kit/src/main/java/net/lzbook/kit/constants/SPKeys.java
+++
@@ -1,8 +0,0 @@
-package net.lzbook.kit.constants;
-
-public interface SPKeys {
-
-    public interface Setting {
-        public static final String AUTO_UPDATE_CAHCE = "AUTO_UPDATE_CAHCE";
-    }
-}
