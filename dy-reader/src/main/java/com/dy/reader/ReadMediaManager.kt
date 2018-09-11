e52b8e05ddcd28c3b4cb2328357aa4ebbe2b6315
weining_yang@dingyuegroup.cn <521314ywn>
2018-09-11 14:19:48 +0800

    修改广告版本

--- dy-reader/src/main/java/com/dy/reader/ReadMediaManager.kt
+++ dy-reader/src/main/java/com/dy/reader/ReadMediaManager.kt
@@ -6,8 +6,6 @@ import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import cn.dycm.ad.nativ.NativeMediaView
-import com.dy.media.MediaCode
 import com.dy.media.MediaControl
 import com.dy.reader.helper.AppHelper
 import com.dy.reader.page.GLReaderView
