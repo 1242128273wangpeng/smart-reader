cf35aa163166d5d677707c1ae048f52fb7a746b9
wanghuilin <huilin_wang@dingyuegroup.cn>
2018-09-18 18:40:41 +0800

    Revert: 书架广告抖动问题修复代码回复

--- bookshelf/src/main/java/com/dingyue/bookshelf/BookShelfADView.kt
+++ bookshelf/src/main/java/com/dingyue/bookshelf/BookShelfADView.kt
@@ -3,7 +3,6 @@ package com.dingyue.bookshelf
 import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import android.widget.FrameLayout
@@ -18,13 +17,11 @@ import com.dy.media.MediaControl
  */
 class BookShelfADView : FrameLayout {
     val activity: Activity
-    var loadedTime = 0L
+
     constructor(act: Activity) : super(act) {
         this.activity = act
         // 修复广告拉取失败时导致无法下拉刷新的问题
         this.minimumHeight = 1
-        //  广告加载出来前默认占位布局
-        addView(LayoutInflater.from(context).inflate(R.layout.item_bookshelf_ad_placeholder, null), LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT))
     }
 
     val preDrawListener = object : ViewTreeObserver.OnPreDrawListener {
@@ -35,22 +32,7 @@ class BookShelfADView : FrameLayout {
         }
     }
 
-    var needRefreshAD = false
-
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
-        if (needRefreshAD) {
-            needRefreshAD = false
-            loadAD()
-        }
-    }
     private fun loadAD() {
-        if (System.currentTimeMillis() - loadedTime < 500) {
-            //放在广告刷新时间过短
-            return
-        }
-        loadedTime = System.currentTimeMillis()
-
         MediaControl.loadBookShelMedia2(activity, object : IMediaControl.MediaCallback {
             override fun requestMediaRepairSuccess(views: List<ViewGroup>) {
 
