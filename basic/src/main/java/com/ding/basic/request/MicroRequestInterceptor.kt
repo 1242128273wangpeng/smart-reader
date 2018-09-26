d32d2d7255568a5eb4b0d094b0d6df30223c762a
CrazyLei <wanglei_wang@dingyuegroup.cn>
2018-09-26 09:25:11 +0800

    1、修改书城刷新方法 2、修改数据流请求拦截器

--- basic/src/main/java/com/ding/basic/request/MicroRequestInterceptor.kt
+++ basic/src/main/java/com/ding/basic/request/MicroRequestInterceptor.kt
@@ -2,19 +2,16 @@ package com.ding.basic.request
 
 import android.text.TextUtils
 import com.ding.basic.Config
+import com.ding.basic.request.MicroService.Companion.AUTH_ACCESS
 import com.ding.basic.token.Token
 import com.orhanobut.logger.Logger
-import okhttp3.FormBody
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 import org.apache.commons.codec.binary.Hex
-import java.net.URL
 import java.net.URLDecoder
-import java.net.URLEncoder
 import org.apache.commons.codec.digest.DigestUtils
 import java.util.*
-
 
 class MicroRequestInterceptor : Interceptor {
 
@@ -22,16 +19,36 @@ class MicroRequestInterceptor : Interceptor {
 
     private fun buildRequestParameters(): Map<String, String> {
         if (requestParameters["packageName"] == null) {
-            requestParameters["os"] = Config.loadRequestParameter("os")
-            requestParameters["udid"] = Config.loadRequestParameter("udid")
-            requestParameters["version"] = Config.loadRequestParameter("version")
-            requestParameters["channelId"] = Config.loadRequestParameter("channelId")
             requestParameters["packageName"] = Config.loadRequestParameter("packageName")
         }
 
-        requestParameters["latitude"] = Config.loadRequestParameter("latitude")
-        requestParameters["cityCode"] = Config.loadRequestParameter("cityCode")
-        requestParameters["longitude"] = Config.loadRequestParameter("longitude")
+        if (requestParameters["os"] == null) {
+            requestParameters["os"] = Config.loadRequestParameter("os")
+        }
+
+        if (requestParameters["udid"] == null) {
+            requestParameters["udid"] = Config.loadRequestParameter("udid")
+        }
+
+        if (requestParameters["version"] == null) {
+            requestParameters["version"] = Config.loadRequestParameter("version")
+        }
+
+        if (requestParameters["channelId"] == null) {
+            requestParameters["channelId"] = Config.loadRequestParameter("channelId")
+        }
+
+        if (requestParameters["latitude"] == null) {
+            requestParameters["latitude"] = Config.loadRequestParameter("latitude")
+        }
+
+        if (requestParameters["longitude"] == null) {
+            requestParameters["longitude"] = Config.loadRequestParameter("longitude")
+        }
+
+        if (requestParameters["cityCode"] == null) {
+            requestParameters["cityCode"] = Config.loadRequestParameter("cityCode")
+        }
 
         if(!TextUtils.isEmpty(Config.loadRequestParameter("loginToken"))){
             requestParameters["loginToken"]=Config.loadRequestParameter("loginToken")
@@ -56,14 +73,14 @@ class MicroRequestInterceptor : Interceptor {
     }
 
     private fun buildRequest(request: Request): Request {
-        var otherRequest = request
+        var interimRequest = request
 
         val parameters = mutableMapOf<String, String>()
 
-        val querySize = otherRequest.url().querySize()
+        val querySize = interimRequest.url().querySize()
 
         for (index in 0 until querySize) {
-            parameters[otherRequest.url().queryParameterName(index)] = otherRequest.url().queryParameterValue(index)
+            parameters[interimRequest.url().queryParameterName(index)] = interimRequest.url().queryParameterValue(index)
         }
 
         if (!parameters.containsKey("packageName")) {
@@ -74,18 +91,21 @@ class MicroRequestInterceptor : Interceptor {
         Logger.e("Sign: $sign")
         parameters["sign"] = sign
 
-        val url = initRequestUrl(otherRequest, parameters) ?: return request
+        val url = initRequestUrl(interimRequest, parameters) ?: return request
 
-        otherRequest = otherRequest.newBuilder()
-                .addHeader("accessKey", Config.loadAccessKey())
+        val builder = interimRequest.newBuilder()
                 .addHeader("publicKey", Config.loadPublicKey())
-                .addHeader("privateKey", Config.loadPrivateKey())
                 .url(url)
-                .build()
 
-        Logger.v("Request: " + otherRequest.url().toString() + " : " + Config.loadPublicKey() + " : " + Config.loadPrivateKey())
+        if (url.contains(AUTH_ACCESS)) {
+            builder.addHeader("accessKey", Config.loadAccessKey())
+        }
 
-        return otherRequest
+        interimRequest = builder.build()
+
+        Logger.v("Request: " + interimRequest.url().toString() + " : " + Config.loadPublicKey() + " : " + Config.loadPrivateKey())
+
+        return interimRequest
     }
 
     private fun loadRequestSign(parameters: Map<String, String>): String {
