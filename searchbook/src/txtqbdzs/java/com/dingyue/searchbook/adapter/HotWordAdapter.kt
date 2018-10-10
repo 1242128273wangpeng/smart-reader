ab6f6e91ef5d61156005be399799152da09ece2b
JoannChen <yongzuo_chen@dingyuegroup.cn>
2018-10-10 15:52:26 +0800

    智胜电子书替 热词改为瀑布流实现

--- searchbook/src/txtqbdzs/java/com/dingyue/searchbook/adapter/HotWordAdapter.kt
+++ searchbook/src/txtqbdzs/java/com/dingyue/searchbook/adapter/HotWordAdapter.kt
@@ -1,17 +1,13 @@
 package com.dingyue.searchbook.adapter
 
-import android.content.Context
-import android.graphics.Color
 import android.support.v4.content.ContextCompat
-import android.text.TextUtils
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
 import com.ding.basic.bean.HotWordBean
 import com.dingyue.searchbook.R
+import kotlinx.android.synthetic.txtqbdzs.item_hot_word.view.*
 import java.util.*
 
 
@@ -21,13 +17,17 @@ import java.util.*
  * Mail yongzuo_chen@dingyuegroup.cn
  * Date 2018/9/20 0020 10:00
  */
-class HotWordAdapter(private var list: List<HotWordBean>) : BaseAdapter() {
+class HotWordAdapter(private var list: List<HotWordBean>,
+                     private val hotWordClickListener: HotWordClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val random: Random? = null
 
     private var oldType = -1
 
-    override fun getCount(): Int {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+            object : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_hot_word, parent, false)) {}
+
+    override fun getItemCount(): Int {
         return if (list.isNotEmpty()) {
             if (list.size >= 5) 6 else list.size
         } else {
@@ -35,61 +35,46 @@ class HotWordAdapter(private var list: List<HotWordBean>) : BaseAdapter() {
         }
     }
 
-    override fun getItem(position: Int): Any? {
-        return null
-    }
-
-    override fun getItemId(position: Int): Long {
-        return 0
-    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val context = parent.context
-        val hotView: View
-        val holder: ViewHolder
-
-        if (convertView == null) {
-            hotView = LayoutInflater.from(context).inflate(R.layout.item_hot_word, parent, false)
-            holder = ViewHolder()
-            holder.hotWordText = hotView.findViewById<View>(R.id.tv_hotword) as TextView
-            hotView.tag = holder
-        } else {
-            hotView = convertView
-            holder = convertView.tag as ViewHolder
-        }
-
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val dataBean = list[position]
-        holder.hotWordText.text = dataBean.keyword
+        holder.itemView.tv_hotWord.text = dataBean.keyword
 
-        var currType = random?.nextInt(7)
-        while (oldType == currType) {
-            currType = random?.nextInt(7)
-        }
-        oldType = currType ?: 0
-        when (currType) {
-            0, 1, 2, 3 -> {
-//                holder.hotWordText.setBackgroundResource(R.drawable.search_hot_word_bg_1)
-                holder.hotWordText.setTextColor(ContextCompat.getColor(context, R.color.search_hot_word_text_color_1))
+        val context = holder.itemView.context
+
+        when (dataBean.superscript) {
+
+            "热" -> {
+                holder.itemView.iv_type.setImageResource(R.drawable.icon_search_hot)
+                holder.itemView.tv_hotWord.setBackgroundResource(R.drawable.draw_search_red_bg)
+                holder.itemView.tv_hotWord.setTextColor(ContextCompat.getColor(context, R.color.search_hot_word_red))
             }
-            4 -> {
-//                holder.hotWordText.setBackgroundResource(R.drawable.search_hot_word_bg_2)
-                holder.hotWordText.setTextColor(ContextCompat.getColor(context, R.color.search_hot_word_text_color_2))
+            "荐" -> {
+                holder.itemView.iv_type.setImageResource(R.drawable.icon_search_recommend)
+                holder.itemView.tv_hotWord.setBackgroundResource(R.drawable.draw_search_orange_bg)
+                holder.itemView.tv_hotWord.setTextColor(ContextCompat.getColor(context, R.color.search_hot_word_orange))
             }
-            5 -> {
-//                holder.hotWordText.setBackgroundResource(R.drawable.search_hot_word_bg_3)
-                holder.hotWordText.setTextColor(ContextCompat.getColor(context, R.color.search_hot_word_text_color_3))
+            "新" -> {
+                holder.itemView.iv_type.setImageResource(R.drawable.icon_search_new)
+                holder.itemView.tv_hotWord.setBackgroundResource(R.drawable.draw_search_green_bg)
+                holder.itemView.tv_hotWord.setTextColor(ContextCompat.getColor(context, R.color.search_hot_word_green))
             }
-            6 -> {
-//                holder.hotWordText.setBackgroundResource(R.drawable.search_hot_word_bg_3)
-                holder.hotWordText.setTextColor(ContextCompat.getColor(context, R.color.search_hot_word_text_color_4))
+
+            else -> {
+                holder.itemView.iv_type.setImageResource(R.drawable.icon_search_transparent)
+                holder.itemView.tv_hotWord.setBackgroundResource(R.drawable.draw_search_gray_bg)
+                holder.itemView.tv_hotWord.setTextColor(ContextCompat.getColor(context, R.color.color_black))
             }
         }
 
-        return hotView
+
+        holder.itemView.setOnClickListener({
+            hotWordClickListener.onHotWordItemClick(it, position, dataBean)
+        })
     }
 
-    private inner class ViewHolder {
-        lateinit var hotWordText: TextView
+
+    interface HotWordClickListener {
+        fun onHotWordItemClick(view: View, position: Int, dataBean: HotWordBean)
     }
 
 }
