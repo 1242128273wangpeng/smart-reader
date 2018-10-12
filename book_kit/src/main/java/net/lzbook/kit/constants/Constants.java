76d08aadb0bbe784d4f134051bc6e0d83ec63dc7
linzhenxiang <linzhenxiang1@126.com>
2018-10-12 19:40:23 +0800

    今日多看 同步问题

--- book_kit/src/main/java/net/lzbook/kit/constants/Constants.java
+++ book_kit/src/main/java/net/lzbook/kit/constants/Constants.java
@@ -430,7 +430,7 @@ public class Constants {
     public static String ad_control_other = "6";//广告分渠道控制广告位    4其他广告
 
 
-    public static Boolean SHARE_SWITCH_ENABLE = true; //是否开启分享功能
+    public static Boolean SHARE_SWITCH_ENABLE = false; //是否开启分享功能
 
 
 
