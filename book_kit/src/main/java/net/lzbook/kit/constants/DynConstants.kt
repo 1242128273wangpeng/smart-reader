07f70cb0480f7d183aa51fb480806fbde01344ed
CrazyLei <wanglei_wang@dingyuegroup.cn>
2018-11-09 09:43:26 +0800

    1、修改问题

--- book_kit/src/main/java/net/lzbook/kit/constants/DynConstants.kt
+++ book_kit/src/main/java/net/lzbook/kit/constants/DynConstants.kt
@@ -5,6 +5,6 @@ package net.lzbook.kit.constants
  */
 object DynConstants {
 
-    const val REQUEST_TIME: Long = 30 * 60 * 1000 // 动态参数请求时间间隔
+    const val REQUEST_TIME: Long = 2 * 60 * 1000 // 动态参数请求时间间隔
 
 }
