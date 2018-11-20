4caf3ba4c8362f9a7ef3cda26fbb80c6bbcae2df
JoannChen <yongzuo_chen@dingyuegroup.cn>
2018-11-20 14:30:28 +0800

    鸿雁替 调试界面添加回归环境地址

--- book_kit/src/main/java/net/lzbook/kit/ui/activity/DebugHostActivity.kt
+++ book_kit/src/main/java/net/lzbook/kit/ui/activity/DebugHostActivity.kt
@@ -108,7 +108,7 @@ class DebugHostActivity : BaseCacheableActivity() {
         } else {
 
             list.add("http://119.254.159.100:8081")
-            list.add("http://8086.zn.bookapi.cn")
+            list.add("http://8011.uzn.bookapi.cn")
             list.add("https://uniontest.bookapi.cn")
             list.add("https://unionapi.bookapi.cn")
             list.add("https://unioncontent.bookapi.cn")
